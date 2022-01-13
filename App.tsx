d2e0bf2559ea6f0b4a0cfcfd8dec47c54e7e58ab
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2022-01-13 14:40:01 -0600

    working example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global";
+
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import WalletConnectProvider from "@walletconnect/react-native-dapp";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import WalletConnectExperience from "./WalletConnectExperience";
+
+const SCHEME_FROM_APP_JSON = "walletconnect-example";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WalletConnectProvider
+      redirectUrl={
+        Platform.OS === "web"
+          ? window.location.origin
+          : `${SCHEME_FROM_APP_JSON}://`
+      }
+      storageOptions={{
+        asyncStorage: AsyncStorage,
+      }}
+    >
+      <View style={styles.container}>
+        <WalletConnectExperience />
+        <StatusBar style="auto" />
+      </View>
+    </WalletConnectProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ WalletConnectExperience.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
+
+const shortenAddress = (address: string) => {
+  return `${address.slice(0, 6)}...${address.slice(
+    address.length - 4,
+    address.length
+  )}`;
+};
+
+export default function WalletConnectExperience() {
+  const connector = useWalletConnect();
+
+  const connectWallet = React.useCallback(() => {
+    return connector.connect();
+  }, [connector]);
+
+  const killSession = React.useCallback(() => {
+    return connector.killSession();
+  }, [connector]);
+
+  return (
+    <>
+      {!connector.connected && (
+        <TouchableOpacity onPress={connectWallet} style={styles.buttonStyle}>
+          <Text style={styles.buttonTextStyle}>Connect a Wallet</Text>
+        </TouchableOpacity>
+      )}
+      {!!connector.connected && (
+        <>
+          <Text>{shortenAddress(connector.accounts[0])}</Text>
+          <TouchableOpacity onPress={killSession} style={styles.buttonStyle}>
+            <Text style={styles.buttonTextStyle}>Log out</Text>
+          </TouchableOpacity>
+        </>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: "#3399FF",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#3399FF",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
