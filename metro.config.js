60b9634a01a1c86437057b03809786547e67b828
Peter Piekarczyk <peter@peterp.me>
2022-01-17 13:54:17 -0600

    use expo-crypto-polyfills

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,5 @@
-const crypto = require.resolve("crypto-browserify");
-const url = require.resolve("url/"); // is forward slash (/) required?
-
 module.exports = {
   resolver: {
-    extraNodeModules: {
-      crypto,
-      url,
-      fs: require.resolve("expo-file-system"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      net: require.resolve("react-native-tcp"),
-      os: require.resolve("os-browserify/browser.js"), // is .js extension required?
-      path: require.resolve("path-browserify"),
-      stream: require.resolve("readable-stream"),
-      vm: require.resolve("vm-browserify"),
-    },
+    extraNodeModules: require("expo-crypto-polyfills"),
   },
 };
